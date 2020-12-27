93ab79e2d025f860414a0322d240c7a1dc3041e5
VuHai <vuhai031194@gmail.com>
2020-12-27 23:05:16 +0700

    Add UpdateSystem + SoundManager

---
+++ Assets/External/UpdateManager/GlobalUpdateSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zitga.Update
+{
+    public class GlobalUpdateSystem : IUpdateSystem
+    {
+        private readonly object _lock = new object();
+        
+        private readonly List<IUpdateSystem> updates;
+        
+        /// <summary>
+        /// Current number update function
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return updates.Count;
+                }
+            }
+        }
+
+        public GlobalUpdateSystem()
+        {
+            updates = new List<IUpdateSystem>();
+        }
+        
+        public void OnUpdate(float deltaTime)
+        {
+            lock (_lock)
+            {
+                foreach (IUpdateSystem update in updates)
+                {
+                    update?.OnUpdate(deltaTime);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add update function from someone object what need to call
+        /// </summary>
+        /// <param name="update"></param>
+        /// <exception cref="Exception"></exception>
+        public void Add(IUpdateSystem update)
+        {
+            lock (_lock)
+            {
+                if (updates.Contains(update))
+                {
+                    throw new Exception($"Object is exist in updates: {nameof(update)}");
+                }
+                updates.Add(update);
+            }
+        }
+        
+        /// <summary>
+        /// remove update from a object when it does not use
+        /// </summary>
+        /// <param name="update"></param>
+        /// <exception cref="Exception"></exception>
+        public void Remove(IUpdateSystem update)
+        {
+            lock (_lock)
+            {
+                if (updates.Contains(update))
+                {
+                    updates.Remove(update);
+                }
+                else
+                {
+                    throw new Exception($"Object is not exist in updates: {nameof(update)}");
+                }
+            }
+        }
+    }
+}
+
